80dda55b5c922c84c06f38dfed94ac4a222008c6
Stiven Tovar <stivent367@gmail.com>
2021-09-24 06:22:34 -0500

    restructuring folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './sass/main.scss';
+import App from './pages/App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './components/features/counter/Counter';
-import './App.scss';
+import logo from '../assets/logo.svg';
+import { Counter } from '../components/features/counter/Counter';
 
 function App() {
   return (
