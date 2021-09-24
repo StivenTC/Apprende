069b449660484d4aac9e7fa0a9f4bb13af1db390
Stiven Tovar <stivent367@gmail.com>
2021-09-24 07:38:14 -0500

    refactor: adding router

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from '../src/pages/App';
+import { Header } from './components/layout/Header/header';
 
 export default function AppRoutes() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={App} />
       </Switch>
