d67615f0e78ce5ddeff1ef5ca0b4f68e4b4f878e
Stiven Tovar <stivent367@gmail.com>
2021-10-07 10:59:29 -0500

    feat(Conclusions): Added solutions pages

--- src/pages/ai/raindrops/Raindrops.js
+++ src/pages/ai/raindrops/Raindrops.js
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Header } from "../../../components/layout/Header/Header";
-import { BiRightArrowAlt } from "react-icons/bi";
 import { useState } from "react";
 import { FeedbackCorrect } from "../../../components/layout/feedback/Feedback";
 import { FeedbackClue } from "../../../components/layout/feedback/FeedbackClue";
