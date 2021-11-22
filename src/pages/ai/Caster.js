5df5e6400b126c9f3509bb6788995e3a5ead78bc
Stiven Tovar <stivent367@gmail.com>
2021-11-22 11:42:53 -0500

    refactor(AI1): Question 5 and routing

--- src/pages/ai/Caster.js
+++ src/pages/ai/Caster.js
@@ -1,15 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Launcher } from './Launcher';
-import { Presentation } from './focus/Presentation';
-import { Focus } from './focus/Focus';
-import { FocusConclusion } from './focus/Conclusion';
-import { SelectQuarterPresentation } from './selectQuarter/Presentation';
-import { SelectQuarter } from './selectQuarter/SelectQuarter';
-import { SelectQuarterConclusion } from './selectQuarter/Conclusion';
-import { RaindropPresentation } from './raindrops/Presentation';
-import { Raindrops } from './raindrops/Raindrops';
-import { RaindropConclusion } from './raindrops/Conclusion';
-import { RaindropQuestion } from './raindrops/Question';
+import Question5 from './question-5/Caster';
 
 
 function CasterAI() {
@@ -20,30 +11,11 @@ function CasterAI() {
   useEffect(() => {
     const changeView = (view) => {
       switch (view) {
+        default:
         case 0:
           return <Launcher goView={setActualView} userData={userData} />;
         case 1:
-          return <Presentation goView={setActualView} saveUser={saveUserData} />;
-        case 2:
-          return <Focus goView={setActualView} saveUser={saveUserData} userData={userData} />;
-        case 3:
-          return <FocusConclusion goView={setActualView} saveUser={saveUserData} />;
-        case 4:
-          return <SelectQuarterPresentation goView={setActualView} saveUser={saveUserData} />;
-        case 5:
-          return <SelectQuarter goView={setActualView} saveUser={saveUserData} userData={userData} />;
-        case 6:
-          return <SelectQuarterConclusion goView={setActualView} saveUser={saveUserData} />;
-        case 7:
-          return <RaindropPresentation goView={setActualView} saveUser={saveUserData} />;
-        case 8:
-          return <Raindrops goView={setActualView} saveUser={saveUserData} userData={userData} />;
-        case 9:
-          return <RaindropConclusion goView={setActualView} saveUser={saveUserData} />;
-        case 10:
-          return <RaindropQuestion goView={setActualView} userData={userData} />;
-        default:
-          return <h1>Gotas</h1>;
+          return <Question5 setActualView={setActualView} />;
       }
     }
 
--- src/pages/ai/Launcher.js
+++ src/pages/ai/Launcher.js
@@ -3,6 +3,7 @@ import HappyEmoji from "../../assets/happyEmoji.png";
 import SadEmoji from "../../assets/sadEmoji.png";
 import { RiArrowRightLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { BiRightArrowAlt } from "react-icons/bi";
 
 export function Launcher({ goView, userData }) {
 
@@ -47,34 +48,12 @@ export function Launcher({ goView, userData }) {
         <img src={robot} alt='robot' />
       </Link>
       <div className="launcher-activities">
-        <div role="button" className={`ai-button ${getClasses("focus")}`} onClick={() => goView(1)}>
-          <p>Reto 1
-            <span><br />Volver a intentarlo</span>
-          </p>
-          <img className="img-complete" src={HappyEmoji} alt='Challenge complete' />
-          <img className="img-failed" src={SadEmoji} alt='Challenge failed' />
-          <RiArrowRightLine />
-        </div>
-        <div role="button" className={`ai-button ${getClasses("selectQuarter")}`} onClick={() => goView(4)}>
-          <p>Reto 2
-            <span><br />Volver a intentarlo</span>
-          </p>
-          <img className="img-complete" src={HappyEmoji} alt='Challenge complete' />
-          <img className="img-failed" src={SadEmoji} alt='Challenge failed' />
-          <RiArrowRightLine />
-        </div>
-        <div role="button" className={`ai-button ${getClasses("rainDrop")}`} onClick={() => goView(7)}>
-          <p>Reto 3
-            <span><br />Volver a intentarlo</span>
-          </p>
-          <img className="img-complete" src={HappyEmoji} alt='Challenge complete' />
-          <img className="img-failed" src={SadEmoji} alt='Challenge failed' />
-          <RiArrowRightLine />
-        </div>
-
-        {validateActivities() && <Link to="/" className="launcher-btn-back">
-          Volver
-        </Link>}
+        <button
+          className="btn-next"
+          onClick={() => goView(1)}>
+          Comenzar
+          <BiRightArrowAlt />
+        </button>
       </div>
     </div>
   );
