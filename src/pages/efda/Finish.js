02768e9534e7e77c4916d7f8c97a83f02af91bc1
Stiven Tovar <stivent367@gmail.com>
2021-11-25 17:53:01 -0500

    Added efda results

--- src/pages/efda/Finish.js
+++ src/pages/efda/Finish.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import robot from "../../assets/Volando.png";
 import FinishBKG from "../../assets/finishBKG.jpg";
 import { useHistory } from 'react-router';
@@ -8,14 +9,38 @@ import { BiRightArrowAlt } from "react-icons/bi";
 export function Finish({ goView, userData }) {
   let history = useHistory();
 
+  const [results] = useState(() => {
+    const lsData = "appData";
+    let correctData = ["D", "C", "C", "C", "D", "B", "B", [2, 5, 6], ["3/10", "2/5", "1/4", "2/2"], "D", "A", "C"]
+    let answers = [];
+    let corrects = 0
+    // getting stored value
+    const saved = localStorage.getItem(lsData);
+    const jsonAnswers = JSON.parse(saved);
+
+    for (var i in jsonAnswers) {
+      answers.push(jsonAnswers[i])
+    }
+    // corrects = answers.map((t, i) => t === correctData[i])
+    for (let [index, val] of answers.entries()) {
+      if (val === correctData[index]) {
+        corrects++
+      }
+    }
+    return `${corrects} de 12 correctas.` || ''
+  });
+
   return (
     <div className="efda-finish" style={{ background: `top/100% 100% no-repeat url("${FinishBKG}")` }}>
       <Link to="/">
         <img src={robot} alt='robot' />
       </Link>
       <div>
-        <p>Has terminado</p>
-
+        <p>¡Has terminado!</p>
+        <br />
+        <br />
+        <p>Resultado</p>
+        <p>{results}</p>
         <button
           className="btn-next"
           onClick={() => history.push("/")}>
